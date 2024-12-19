a9b5f56ce87dab45f00f387897f685f3c68aa3fd
Vaibhav Chaudhary <vaibhavchaudhary898@gmail.com>
2024-12-19 23:02:46 +0530

    added movement

--- player.js
+++ player.js
@@ -5,10 +5,24 @@ export class Player{
         this.width = 100;
         this.x = 0;
         this.y = this.game.height - this.height;
+        this.vy = 0;
+        this.weight = 1;
         this.image = document.getElementById('player');
+        this.speed = 0;
+        this.maxSpeed = 10;
     }
-    update(){
-        //this.x++;
+    update(input){
+        this.x += this.speed;
+        if(input.includes('ArrowRight')) this.speed = this.maxSpeed;
+        else if (input.includes('ArrowLeft'))this.speed = -this.maxSpeed;
+        else this.speed = 0;
+        if(this.x < 0)this.x = 0;
+        if(this.x > this.game.width - this.width)this.x = this.game.width - this.width;
+        //verical movement
+        if(input.includes('ArrowUp') && this.onGround())this.vy -= 20;
+        this.y += this.vy;
+        if(!this.onGround())this.vy += this.weight;
+        else this.vy = 0;
     }
     draw(context){
         context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
@@ -16,5 +30,8 @@ export class Player{
         //drawimage is used to draw an image from a source to a destination in a web page
         //sx -> start x coord in source frame, sy -> start y coord in source frame
         //sw -> source width sh -> height, these four select the frame from source image
+    }
+    onGround(){
+        return this.y > this.game.height - this.height;
     }
 }
